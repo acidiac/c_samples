c4410e0f3077fc135a9473efeef3f4cec902d66c
Amit Chanchal <amitchanchal@Amits-MacBook-Air.local>
2016-09-27 16:44:09 +0530

    few more exercises

--- cf_c/cf_c12273.c
+++ cf_c/cf_c12273.c
@@ -10,3 +10,24 @@
  * and print as a "float" the equivalent temperature in degrees Fahrenheit. Print your results in a form such as
  * 100.0 degrees Celsius converts to 212.0 degrees Fahrenheit.
  */
+// Preprocessor directives, includes
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//forward declaration
+float ctof (float celcius);
+
+int main(){
+	float result = ctof(100.0);
+	printf("%.1f degrees Fahrenheit\n", result);
+	return 0;
+}
+
+
+
+float ctof (float celcius){
+	float far;
+	far = (celcius*9.0/5.0) + 32;
+	return far;
+}
